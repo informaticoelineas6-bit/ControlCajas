878e2ead23ae007b539327d68fadb4b63dab43bb
josemag <josemag@uci.cu>
2026-03-23 09:40:16 -0400

    Minor fix

--- next.config.js
+++ next.config.js
@@ -1,4 +1,4 @@
-const { setupDevPlatform } = require("@cloudflare/next-on-pages/next-dev");
+import { setupDevPlatform } from "@cloudflare/next-on-pages/next-dev";
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
@@ -9,6 +9,6 @@ if (process.env.NODE_ENV === "development") {
   await setupDevPlatform();
 }
 
-module.exports = nextConfig;
+export default nextConfig;
 
 import("@opennextjs/cloudflare").then((m) => m.initOpenNextCloudflareForDev());
