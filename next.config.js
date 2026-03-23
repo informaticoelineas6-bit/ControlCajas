e2dcde6200e9a4b2455f45b2be6725aff395db0f
josemag <josemag@uci.cu>
2026-03-23 09:38:04 -0400

    Cloudflare migrate

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
+const { setupDevPlatform } = require("@cloudflare/next-on-pages/next-dev");
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
 };
 
+if (process.env.NODE_ENV === "development") {
+  await setupDevPlatform();
+}
+
 module.exports = nextConfig;
+
+import("@opennextjs/cloudflare").then((m) => m.initOpenNextCloudflareForDev());
